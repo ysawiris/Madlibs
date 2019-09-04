2c3aa3a5ba59a69b3ddc428731f63665de06300c
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-09-03 17:54:04 -0700

    [final]

--- MadLibs-Youssef-Sawiris.py
+++ MadLibs-Youssef-Sawiris.py
@@ -6,14 +6,15 @@ Mad Libs by Youssef Sawiris
 a  = [] #list for Adjectives
 n  = [] #list for Nouns
 ad = [] #list for Adverbs
+e  = [] #list for Exclamation
 
 
 def putHead():
     print("\n")
-    print("Welcome to Youssef's Madlibs.")
+    print("\u001b[36mWelcome to Youssef's Madlibs.")
     print("We have a very exciting story to tell you!")
     print("We need your help to finish it!")
-    print("Please fill out the questionaire to hear our story!")
+    print("Please fill out the questionaire to hear our story!\u001b[0m")
     print("\n")
 
 
@@ -30,25 +31,27 @@ def getNoun():
     n.append(input("Type in a girl's name: "))
     n.append(input("Type in a name of place: "))
 
+
 def getAdverb():
     #this fuction will collect adverbs from user
     ad.append(input("Type in an adverb: "))
     ad.append(input("Type in another adverb: "))
 
+def getExclamation():
+    #this function will collect Exclamation from user
+    e.append(input("Type in a exclamation: "))
+
 def putStory():
     print("\n")
-    print("It started like any other day.")
-    print(n[0],"called me right around 9, but this time they had")
-    print(a[0], "news.", n[1], " was in a fight with their")
-    print(a[1], "boyfriend. I ", ad[0], "ran to my car and started")
+    print(e[0], ", it started like any other day.")
+    print(n[0],"called me right around 9 am, but this time they had")
+    print(a[0], "news.", n[1], "was in a fight with their")
+    print(a[1], "boyfriend. I", ad[0], "ran to my car and started")
     print("driving to", n[2], ". When I got there, I found out")
-    print(n[0], "and", n[1], ad[1], "threw me a ", a[2], "party!")
-    print("Don't I have", a[3]," friends!")
-    print("Luckly they brought white claws!")
+    print(n[0], "and", n[1], ad[1], "threw me a", a[2], "party!")
+    print("Don't I have", a[3],"friends!")
+    print("\u001b[33mLuckily they brought white claws!\u001b[0m")
     print("\n")
-
-
-
 
 #calling fuctions
 putHead()
@@ -58,5 +61,7 @@ getAdjective()
 getNoun()
 
 getAdverb()
+
+getExclamation()
 
 putStory()
